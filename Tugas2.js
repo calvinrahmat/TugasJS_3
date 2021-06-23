7996b466cac889d18bbcfdc0d8209a288951a810
calvinrahmat <calvin.rahmat@gmail.com>
2021-06-23 12:10:06 +0700

    fix bug

--- Tugas2.js
+++ Tugas2.js
@@ -1,29 +1,41 @@
-const getmonth = (callback)=>{
-    setTimeout(()=>{
-        let error= true
-        let month = ['January','February','March','April','May','June','July','August','September','October','November','December']
-        if(!error){
-            callback(null, month)
-        }else{
-            callback(new Error('Sorry data not found',[]))
-        }
-    },4000)
-}
+const getmonth = (callback) => {
+	setTimeout(() => {
+		let error = false;
+		let month = [
+			'January',
+			'February',
+			'March',
+			'April',
+			'May',
+			'June',
+			'July',
+			'August',
+			'September',
+			'October',
+			'November',
+			'December',
+		];
+		if (!error) {
+			callback(null, month);
+		} else {
+			callback(new Error('Sorry data not found', []));
+		}
+	}, 4000);
+};
 
-const displayMonth = async (error,month)=>{
-    try{
-    num=10
-        let convert= month.map((y)=>y.toUpperCase())
-        
-        for (let i=0; i<=num;i++){
-        console.log(convert[i])
-        
-    }}catch{
-        {
-        console.log(error)
-        }
-    }
-}
+const displayMonth = async (error, month) => {
+	try {
+		num = 10;
+		let convert = month.map((y) => y.toUpperCase());
 
+		for (let i = 0; i <= num - 1; i++) {
+			console.log(convert[i]);
+		}
+	} catch {
+		{
+			console.log(error);
+		}
+	}
+};
 
-getmonth(displayMonth)
+getmonth(displayMonth);
