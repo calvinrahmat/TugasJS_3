274eb7d7e340bfede6f7819f416c4eef3d9bb3da
calvinrahmat <calvin.rahmat@gmail.com>
2021-06-23 13:12:07 +0700

    cleaning up

--- Tugas5.js
+++ Tugas5.js
@@ -3,8 +3,12 @@ const api_url = 'https://jsonplaceholder.typicode.com/users';
 
 async function dataPerson() {
 	const res = await fetch(api_url);
+	if (res.ok) {
+		console.log('fetch success!');
+	} else {
+		console.log('fetch failed!');
+	}
 	const data = await res.json();
-	console.log(data);
 	let result = data.map((d) => d.name);
 	console.log(result);
 }
