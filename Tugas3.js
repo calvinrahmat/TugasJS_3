398bfb28c4b6b610001fe5862719100bf45491ca
calvinrahmat <calvin.rahmat@gmail.com>
2021-06-23 13:11:51 +0700

    cleaning up

--- Tugas3.js
+++ Tugas3.js
@@ -1,12 +1,13 @@
+let randomChar = '';
+let randomNumber = '';
+let symbol = '!@#$';
+let char = 'abcdefghijklmnopqrstuvwxyz';
+let numbers = '123456789';
+let randomIndexSymbol = Math.floor(Math.random() * symbol.length);
+let randomSymbol = symbol.charAt(randomIndexSymbol);
+
 function checkPass(pass, level) {
-	let randomChar = '';
-	let randomNumber = '';
 	let length = pass.length;
-	let symbol = '!@#$';
-	let char = 'abcdefghijklmnopqrstuvwxyz';
-	let numbers = '123456789';
-	let randomIndexSymbol = Math.floor(Math.random() * symbol.length);
-	let randomSymbol = symbol.charAt(randomIndexSymbol);
 	let newPass =
 		pass.charAt(0).toUpperCase() +
 		pass.slice(1, length - 1) +
@@ -14,15 +15,12 @@ function checkPass(pass, level) {
 	for (let i = 1; i <= 6 - pass.length; i++) {
 		randomChar += char.charAt(Math.floor(Math.random() * char.length));
 	}
-	for (let i = 0; i <= pass; i++) {
-		newPass += pass.charAt(Math.floor(Math.random())).toUpperCase;
-	}
 	for (let i = 0; i < 2; i++) {
 		randomNumber += numbers.charAt(Math.floor(Math.random() * numbers.length));
 	}
 
 	if (level === 'low') {
-		if (pass.length < 6) {
+		if (length < 6) {
 			result = pass + randomChar + randomNumber;
 			console.log(result);
 		} else {
@@ -31,7 +29,7 @@ function checkPass(pass, level) {
 		}
 	}
 	if (level === 'good') {
-		if (pass.length <= 6) {
+		if (length <= 6) {
 			let randomCharGood = '';
 			for (let i = 1; i <= 6 - pass.length; i++) {
 				randomCharGood += char.charAt(Math.floor(Math.random() * char.length));
