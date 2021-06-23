b10489d3c453015409d578d3d768157c59bbc5e6
calvinrahmat <calvin.rahmat@gmail.com>
2021-06-23 12:10:13 +0700

    fix bug

--- Tugas3.js
+++ Tugas3.js
@@ -1,29 +1,58 @@
-function checkPass (pass, level)
-{   let low = ''
-    let good = ''
-    let strong = ''
-    let numbers = null
-    let length = pass.length
-    let symbol = '!@#$'
-    let randomIndex=Math.floor(Math.random() * 4);
-    let randomNumbers=Math.floor(Math.random() * 1000)
-    let randomSymbol=symbol.charAt(randomIndex)
-    
-    if (level==='low'){
-        result= pass+randomNumbers
-        console.log(result)
-        
-    }
-    if(level === 'good'){
-        result = pass.charAt(0).toUpperCase()+pass.slice(1)+randomNumbers
-        console.log(result)
-    }
+function checkPass(pass, level) {
+	let randomChar = '';
+	let randomNumber = '';
+	let length = pass.length;
+	let symbol = '!@#$';
+	let char = 'abcdefghijklmnopqrstuvwxyz';
+	let numbers = '123456789';
+	let randomIndexSymbol = Math.floor(Math.random() * symbol.length);
+	let randomSymbol = symbol.charAt(randomIndexSymbol);
+	let newPass =
+		pass.charAt(0).toUpperCase() +
+		pass.slice(1, length - 1) +
+		pass.charAt(length - 1).toUpperCase();
+	for (let i = 1; i <= 6 - pass.length; i++) {
+		randomChar += char.charAt(Math.floor(Math.random() * char.length));
+	}
+	for (let i = 0; i <= pass; i++) {
+		newPass += pass.charAt(Math.floor(Math.random())).toUpperCase;
+	}
+	for (let i = 0; i < 2; i++) {
+		randomNumber += numbers.charAt(Math.floor(Math.random() * numbers.length));
+	}
 
-    if(level == 'strong'){
-        result = pass.charAt(0).toUpperCase()+pass.slice(1,length-1)+pass.charAt(length-1).toUpperCase()+randomSymbol+randomNumbers
-        console.log(result)
-    }
-   
+	if (level === 'low') {
+		if (pass.length < 6) {
+			result = pass + randomChar + randomNumber;
+			console.log(result);
+		} else {
+			result = pass + randomNumber;
+			console.log(result);
+		}
+	}
+	if (level === 'good') {
+		if (pass.length <= 6) {
+			let randomCharGood = '';
+			for (let i = 1; i <= 6 - pass.length; i++) {
+				randomCharGood += char.charAt(Math.floor(Math.random() * char.length));
+			}
+			randomCharGood = newPass + randomCharGood + randomNumber;
+			console.log(randomCharGood);
+		} else {
+			result = pass + randomNumber;
+			console.log(result);
+		}
+	}
+
+	if (level === 'strong') {
+		if (pass.length <= 6) {
+			result = newPass + randomSymbol + randomChar + randomNumber;
+			console.log(result);
+		} else {
+			result = newPass + randomSymbol + randomNumber;
+			console.log(result);
+		}
+	}
 }
 
-checkPass('abcd','strong')
+checkPass('abcd', 'strong');
