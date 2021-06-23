467e0e49848072c9e9f9c28942005b0ca16c51ee
calvinrahmat <calvin.rahmat@gmail.com>
2021-06-23 21:28:16 +0700

    fix

--- Tugas6.js
+++ Tugas6.js
@@ -1,30 +1,23 @@
 const fetch = require('node-fetch');
-const api_url = 'https://reqres.in/api/users/4';
+const data_api = 'https://reqres.in/api/users/4';
 
 let data = {
 	id: 4,
-	email: 'eve.holt@reqres.in',
+	email: 'astrowarden@moba.ml',
 	first_name: 'Yve',
 	last_name: 'astrowarden',
 	avatar: 'https://reqres.in/',
 };
 
-async function api() {
-	const res = await fetch(api_url);
-	const data_api = await res.json();
-	return data_api;
-}
-
-let data2 = api();
-
 async function objectCompare(data1, data2) {
-	let originalData = Object.entries(data1);
+	const value = Object.values(data1);
+	const res = await fetch(data2);
+	const data_api = await res.json();
+	const value_api = Object.values(data_api.data);
 
-	let apiData = Object.entries(data2);
-
-	for (let i = 0; i < originalData.length; i++) {
-		let array1 = originalData[i];
-		let array2 = apiData[i];
+	for (let i = 0; i < value.length - 1; i++) {
+		let array1 = value[i];
+		let array2 = value_api[i];
 		if (array1 != array2) {
 			result = 'false';
 		} else {
@@ -34,4 +27,4 @@ async function objectCompare(data1, data2) {
 	console.log(result);
 }
 
-objectCompare(data, data2);
+objectCompare(data, data_api);
